7cfa5e65a725fa77fe7a1cbb31320166664f2be3
mahendrarajdhami <mahendrarajdhami@gmail.com>
2018-05-18 07:14:12 +0545

    Added Initial Files

---
+++ circularindicator/src/main/java/com/deltatechnepal/circularindicator/DefaultAnimatorListener.java
@@ -0,0 +1,40 @@
+package com.deltatechnepal.circularindicator;
+
+import android.animation.Animator;
+
+/**
+ * Created by DeltaTech on 5/16/2018.
+ */
+
+
+class DefaultAnimatorListener implements Animator.AnimatorListener {
+    @Override
+    public void onAnimationStart(Animator animation, boolean isReverse) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animation, boolean isReverse) {
+
+    }
+
+    @Override
+    public void onAnimationStart(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animation) {
+
+    }
+}
